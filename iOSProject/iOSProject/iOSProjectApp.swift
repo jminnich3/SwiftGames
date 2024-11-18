b1339b909a5f916e1ab2d98945d75d0fe58277f6
jminnich3 <joshminnich3@gmail.com>
2024-11-18 16:30:43 -0500

    first commit
    blank project

---
+++ iOSProject/iOSProject/iOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSProjectApp.swift
+//  iOSProject
+//
+//  Created by COMP401 on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSProject/iOSProjectTests/iOSProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSProjectTests.swift
+//  iOSProjectTests
+//
+//  Created by COMP401 on 11/18/24.
+//
+
+import Testing
+@testable import iOSProject
+
+struct iOSProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
